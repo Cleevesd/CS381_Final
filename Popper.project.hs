f73d13a67150a871ee92ee00ae57af33e80e082a
ryansisco <ryansisco8@gmail.com>
2020-02-27 19:37:59 -0800

    updated abstract syntax

--- Popper.project.hs
+++ Popper.project.hs
@@ -1,1 +1,33 @@
 module Popper where
+
+import Prelude hiding (Num)
+
+-- 
+-- POPPER SYNTAX
+--
+-- num      ==  integer
+-- bool     ==  `true` or `false`
+-- string   ==  standard string
+-- prog     ==  [cmd, cmd, cmd...] 
+-- cmd      ==  num
+--          ==  bool
+--          ==  string
+--          ==  `multiply`
+--          ==  `divide`
+--          ==  `add`
+--          ==  `subtract`
+--          ==  `tothe` (This command is for exponents like 2 tothe 3 results in 8)
+--          ==  `mod`   (This is modulo, will be done with recursion until lessthan then return)
+
+type Prog = [Cmd]
+
+data Cmd = PushN Int
+         | PushB Bool
+         | PushS String
+         | Multiply
+         | Divide
+         | Add
+         | Subtract
+         | Tothe
+         | Mod
+  deriving (Eq, Show)
